d9ae94a20436135f99142c472d4d643d71242c66
Stefano Braghin <stefano.braghin@gmail.com>
2013-05-22 16:24:43 +0100

    fix push_all.sh

--- bash/push_all.sh
+++ bash/push_all.sh
@@ -2,9 +2,13 @@
 
 for x in $(ls $1); do
     if [ -d $x -a -d $x/.git ]; then
-	git status | 
-	echo "Pushing $x"
 	cd $x
-	git push
+	if [ $(git status | grep -q 'Your branch is ahead') ]; then
+	    echo "Pushing $x"
+	    #git push > /dev/null 2>&1
+	else
+	    echo "$x is updated"
+	fi
+	cd -
     fi
 done
