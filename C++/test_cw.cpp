af100e222d80debb00bf3a995398c6f9ce47a448
Stefano Braghin <stefano.braghin@gmail.com>
2013-06-04 10:24:25 +0100

    modified test_CW

--- C++/test_cw.cpp
+++ C++/test_cw.cpp
@@ -4,13 +4,15 @@
 int main(int argc, char **argv) {
   uint64_t ui64 = 1;
   unsigned long tl = 1;
+  unsigned ti = 1;
 
   std::cout << "sizeof(int):      " << sizeof(int) << '\n';
   std::cout << "sizeof(long):     " << sizeof(long) << '\n';
   std::cout << "sizeof(uint64_t): " << sizeof(uint64_t) << '\n';
 
-  for (int i = 0; i < 64; ++i) {
-    std::cout << "uint_64 " << (ui64 <<=1) << '\n';
-    std::cout << "long    " << (tl <<= 1) << '\n';
+  for (int i = 1; i <= 64; ++i) {
+    std::cout << "i: " << i << " uint_64      " << (ui64 <<= 1) << '\n';
+    std::cout << "i: " << i << " long         " << (tl <<= 1) << '\n';
+    std::cout << "i: " << i << " unsigned int " << (ti <<= 1) << '\n';
   }
 }
