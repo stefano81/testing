f85fe594f806419bc91fb73cf8c9420df380bc16
Stefano Braghin <stefano.braghin@gmail.com>
2013-06-14 16:12:44 +0100

    add a script to decect old plugins version for eclipse

---
+++ python/detect_old_eclipse_plugin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+def main():
+    if 2 != len(sys.argv):
+        print 'Usage:', sys.argv[0], ' <eclipse plugin directory>'
+        return
+
+    versions = {}
+
+    for plugin in os.listdir(sys.argv[1]):
+        parts = plugin.split("_", 1);
+        versions.setdefault(parts[0], []).append(parts[1])
+
+    for plugin in sorted(versions):
+        if 1 < len(versions[plugin]):
+            for version in sorted(versions[plugin], reverse=True)[1:]:
+                print '{}_{}'.format(plugin, version)
+
+if __name__ == '__main__':
+    main()
