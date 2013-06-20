bc7a2fd1efbb95ddad45a652230afe5573ee5ebe
Stefano Braghin <stefano.braghin@gmail.com>
2013-06-20 17:36:36 +0100

    improved

--- python/draw_grammar_graph.py
+++ python/draw_grammar_graph.py
@@ -2,6 +2,11 @@
 
 import sys
 
+def sanitaze(line):
+    if line:
+        line = line.strip().replace('<', ' ').replace('>', ' ').replace('|', ' ').replace('.', ' ').replace('"', '').replace(':', ' ')
+    return line
+
 '''
 Usage: dwar_grammar_graph <grammar.jj> | dot
 '''
@@ -13,23 +18,24 @@ def main():
     with open(sys.argv[1]) as gf:
         for line in gf:
             if ':' in line:
-                for token in line.split(' '):
-                    if '()' in token:
-                        cat = token[:len(token)-2]
-                        #relations[cat] = set()
+                for token in sanitaze(line).split(' '):
+                    if '(' in token:
+                        cat = token[:token.find('(')].strip()
+                        relations[cat] = set()
+                        break
                 continue
-
-            for token in line.strip().replace('<', ' ').replace('>', ' ').replace('|', ' ').split(' '):
+            if not cat:
+                continue
+            for token in sanitaze(line).split(' '):
                 if len(token) > 1 and '(' in token and ')' in token:
-                    #relations[cat].add(token[:token.find('(')])
-                    relations.setdefault(token[:token.find('(')], []).append(cat)
-                print
+                    relations[cat].add(token[:token.find('(')].strip())
 
     print 'digraph', sys.argv[1].split('/')[-1].split('.')[0], '{'
     for a in relations:
         if len(a) and ';' not in a and '}' not in a:
             for b in set(relations[a]):
-                print b,'->',a,';'
+                if len(b):
+                    print a,'->',b,';'
     print '}'
 
 if __name__ == '__main__':
