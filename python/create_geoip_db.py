eaa3c51805fcf50776d54a89a70870f3cfb2d5ab
Stefano Braghin <stefano.braghin@gmail.com>
2013-09-12 15:30:08 +0100

    fix an error in the generated sql

--- python/create_geoip_db.py
+++ python/create_geoip_db.py
@@ -1,23 +1,25 @@
-##!/usr/bin/env python
+#!/usr/bin/env python
 
 import sys
 import csv
 
 def main():
     print 'create table country (id integer primary key, country_code text);'
-    print 'create table ip (start integer, end integer, country integer);'
+    print 'create table ip (id integer, start integer, end integer);'
     print 'create table command (id integer, command text);'
 
     with open(sys.argv[1], r'r') as datafile:
         csv_reader = csv.reader(datafile)
         countries = {}
         for record in csv_reader:
-            countries.setdefault(record[4], []).append((int(record[2]), int(record[3])))
+            countries.setdefault(record[4], []).append((record[2], record[3]))
 
         i = 0
         for k in sorted(countries):
-            print 'insert into country values ('+str(i)+', "'+k'");'
-            #for (s, e) in countries[k]:
+            print 'insert into country (id, country_code) values (' + str(i) + ', "' + k + '");'
+            for (s, e) in countries[k]:
+                print 'insert into ip (id, start, end) values (' + str(i) + ', ' + s + ', ' + e + ');'
+            i = i + 1
 
 if __name__ == '__main__':
     main()
